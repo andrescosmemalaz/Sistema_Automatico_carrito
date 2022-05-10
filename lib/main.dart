7bfcf9db3951362606abee41b7c537a283adb32b
AndresCosme <maximo.cosme.malasquez@gmail.com>
2022-05-10 01:43:17 -0500

    day2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_application_1/pages/home_page.dart';
 import 'package:flutter_application_1/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +14,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.red),
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       darkTheme: ThemeData(brightness: Brightness.dark),
       initialRoute: "/home",
       routes: {
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,16 +4,43 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Página de Ingreso",
-          style: TextStyle(
-            fontSize: 20,
-            color: Color.fromARGB(253, 119, 236, 73),
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    );
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login_image_2.png",
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              "Bienvenido",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText: "Ingresé Nombre de Usuario",
+                        labelText: "Nombre de Usuario"),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        hintText: "Ingresé Contraseña",
+                        labelText: "Contraseña de Usuario"),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ));
   }
 }
